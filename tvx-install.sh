67bea389a067787e84da771dc0cb2cd8d75aca05
Tobi <tobid7@outlook.de>
2023-02-26 19:18:00 +0100

    Update tvx-install.sh

--- tvx-install.sh
+++ tvx-install.sh
@@ -52,30 +52,25 @@ echo "2. AMD"
 echo "3. Intel"
 echo "4. AMD ROCm"
 read -p "Enter your choice (1, 2, 3, or 4): " choice
-
-if ! [[ "$choice" =~ ^[1-4]$ ]] ; then
-  echo "Invalid choice. Exiting..."
-  exit 1
-elif [ $choice -eq 1 ]
-then
-  echo "Installing NVIDIA drivers..."
-  sudo pacman -S nvidia nvidia-utils nvidia-settings
-  sudo systemctl enable nvidia-persistenced
-  sudo systemctl enable nvidia-fallback
-elif [ $choice -eq 2 ]
-then
-  echo "Installing AMD drivers..."
-  sudo pacman -S mesa vulkan-radeon libva-mesa-driver libva-vdpau-driver
-  sudo pacman -S lib32-mesa lib32-vulkan-radeon lib32-libva-mesa-driver lib32-libva-vdpau-driver
-elif [ $choice -eq 3 ]
-then
-  echo "Installing Intel drivers..."
-  sudo pacman -S mesa libva-intel-driver vulkan-intel
-  sudo pacman -S lib32-mesa lib32-libva-intel-driver lib32-vulkan-intel
-elif [ $choice -eq 4 ]
-then
-  echo "Installing AMD ROCm drivers..."
-  sudo pacman -S rocm-libs rocm-dev rocm-utils
+if [ "$choice" = "1" ]; then
+    echo "Installing NVIDIA drivers..."
+    sudo pacman -S nvidia nvidia-utils nvidia-settings
+    sudo systemctl enable nvidia-persistenced
+    sudo systemctl enable nvidia-fallback
+elif [ "$choice" = "2" ]; then
+    echo "Installing AMD drivers..."
+    sudo pacman -S mesa vulkan-radeon libva-mesa-driver libva-vdpau-driver
+    sudo pacman -S lib32-mesa lib32-vulkan-radeon lib32-libva-mesa-driver lib32-libva-vdpau-driver
+elif [ "$choice" = "3" ]; then
+    echo "Installing Intel drivers..."
+    sudo pacman -S mesa libva-intel-driver vulkan-intel
+    sudo pacman -S lib32-mesa lib32-libva-intel-driver lib32-vulkan-intel
+elif [ "$choice" = "4" ]; then
+    echo "Installing AMD ROCm drivers..."
+    sudo pacman -S rocm-opencl rocm-libs
+else
+    echo "Invalid choice. Exiting..."
+    exit 1
 fi
 
 echo "Setting timezone"
