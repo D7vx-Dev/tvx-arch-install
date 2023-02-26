e06697f6187cd5779401ba0b19412b2f8bbb2dc2
Tobi <tobid7@outlook.de>
2023-02-26 19:23:51 +0100

    Update tvx-install.sh

--- tvx-install.sh
+++ tvx-install.sh
@@ -43,31 +43,29 @@ echo "Generate chroot script"
 cat > /mnt/tvx-chroot.sh << EOF
 #!/bin/bash
 
-
-#!/bin/bash
-
 echo "Which GPU driver do you want to install?"
 echo "1. NVIDIA"
 echo "2. AMD"
 echo "3. Intel"
 echo "4. AMD ROCm"
 read -p "Enter your choice (1, 2, 3, or 4): " choice
-if [ "$choice" = "1" ]; then
+
+if [[ \$choice == 1 ]]; then
     echo "Installing NVIDIA drivers..."
     sudo pacman -S nvidia nvidia-utils nvidia-settings
     sudo systemctl enable nvidia-persistenced
     sudo systemctl enable nvidia-fallback
-elif [ "$choice" = "2" ]; then
+elif [[ \$choice == 2 ]]; then
     echo "Installing AMD drivers..."
     sudo pacman -S mesa vulkan-radeon libva-mesa-driver libva-vdpau-driver
     sudo pacman -S lib32-mesa lib32-vulkan-radeon lib32-libva-mesa-driver lib32-libva-vdpau-driver
-elif [ "$choice" = "3" ]; then
+elif [[ \$choice == 3 ]]; then
     echo "Installing Intel drivers..."
     sudo pacman -S mesa libva-intel-driver vulkan-intel
     sudo pacman -S lib32-mesa lib32-libva-intel-driver lib32-vulkan-intel
-elif [ "$choice" = "4" ]; then
+elif [[ \$choice == 4 ]]; then
     echo "Installing AMD ROCm drivers..."
-    sudo pacman -S rocm-opencl rocm-libs
+    sudo pacman -S libclc rocm-clang hsa-rocr-dev rocm-dev rocm-utils
 else
     echo "Invalid choice. Exiting..."
     exit 1
